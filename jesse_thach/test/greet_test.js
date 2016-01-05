b800f6b66e37e9327fbc2283ebf510d6f1d4fa95
jessethach <jessethach@gmail.com>
2016-01-05 11:43:19 -0800

    initial commit

---
+++ jesse_thach/test/greet_test.js
@@ -0,0 +1,14 @@
+var greet = require(__dirname + '/../greet');
+var expect = require('chai').expect;
+
+describe('the greet function', function() {
+  it('should greet a name', function() {
+    expect(greet('jesse')).to.eql('hello jesse');
+  });
+});
+
+describe('the command line greet', function() {
+  it('should show process argv', function() {
+    expect(greet()).to.eql('hello ' + undefined);
+  });
+});
