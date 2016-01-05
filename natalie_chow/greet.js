c181fa5011f6455ad58f36f5a9da5217e424902c
Natalie Chow <cychownat@gmail.com>
2016-01-05 10:25:13 -0800

    Bonus 1: run greet with cmd line argument

--- natalie_chow/greet.js
+++ natalie_chow/greet.js
@@ -1,3 +1,14 @@
-exports.greet = function(name) {
+var greet = function(name) {
   return 'hello ' + name;
 };
+
+exports.handleConsole = (function(cmdArgs) {
+  if (cmdArgs.length > 2) {
+    var args = cmdArgs.slice(2);
+    args.forEach(function(arg) {
+      console.log(greet(arg));
+    });
+  }
+})(process.argv);
+
+exports.greet = greet;
