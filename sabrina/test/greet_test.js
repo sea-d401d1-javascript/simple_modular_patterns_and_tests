4c9bc8ac4c9df926130a7c19c7e108cd1e50f9ab
SABRINA TEE <sabrinatee@me.com>
2016-01-05 08:52:56 -0800

    Added test files

--- sabrina/test/greet_test.js
+++ sabrina/test/greet_test.js
@@ -1,9 +1,9 @@
-var greet = require(__dirname +  '/../greet');
+var greet_test = require(__dirname +  '/../greet');
 
 var expect = require('chai').expect;
 
-describe('the greet function', function() {
+describe('greet function', function() {
     it('should greet a name', function() {
-        expect(greet('test')).to.eql('hello test');
+        expect(greet_test('name')).to.eql('hello name');
     });
 });
