1481765c990f70184cb19d010a3be1ab2bdb57d9
Cameron Forras <cameronforras@hotmail.com>
2016-01-06 12:05:18 -0800

    added gulp and extras.

--- Cameron Forras/test/greet_test.js
+++ Cameron Forras/test/greet_test.js
@@ -2,7 +2,7 @@ var greet = require(__dirname + '/../lib/greet').greet;
 var expect = require('chai').expect;
 
 describe('the greet function', function() {
-	it('should greet the person\'s name', function() {
-		expect(greet('test')).to.eql('hello test');
-	});
+    it('should greet the person\'s name', function() {
+        expect(greet('test')).to.eql('hello test');
+    });
 });
