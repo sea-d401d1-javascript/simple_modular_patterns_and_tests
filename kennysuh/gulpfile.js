7ea701b50becee75812db984bac1d6a3e9319bdf
suhk <kennethgenesuh@gmail.com>
2016-01-06 13:15:38 -0800

    bonuspoint

--- kennysuh/gulpfile.js
+++ kennysuh/gulpfile.js
@@ -4,7 +4,32 @@ var files = ['**/*.js', '!node_modules/**'];
 
 gulp.task('lint', function() {
   return gulp.src(files)
-    .pipe(eslint())
+    .pipe(eslint({
+      "settings": {
+        "ecmascript": 5
+      },
+      "ecmaFeatures": {
+      },
+      "env": {
+        "browser": true,
+        "jquery": true
+      },
+      "rules": {
+        "no-console": 0,
+        "semi": 1,
+        "strict": 0,
+        "indent": [2, 2],
+        "quotes": [1, "single"],
+        "no-multi-spaces": [1, {
+          "exceptions": {
+            "VariableDeclarator": true,
+            "FunctionExpression": true
+          }
+        }],
+        "key-spacing": [0, {"align": "value"}],
+        "no-underscore-dangle": 0
+      }
+    }))
     .pipe(eslint.format());
 });
 
