202e44a3e6f0a274e57768568c44f61c0e60b137
Miranda Raymond <mirandaraymond@me.com>
2016-01-06 10:39:52 -0800

    added a console log, then deleted it to test the watch task

--- miranda-raymond/hello.js
+++ miranda-raymond/hello.js
@@ -1,9 +1,9 @@
 var greet = require(__dirname + '/lib/greet');
 
 var hello = exports = module.exports = function() {
-  var greeting = greet(process.argv[2] || 'world')); //argv is argument vector an array like structure
+  var greeting = greet(process.argv[2] || 'world'); //argv is argument vector an array like structure
   console.log(greeting);
   return greeting;
-}
+};
 
 hello();
