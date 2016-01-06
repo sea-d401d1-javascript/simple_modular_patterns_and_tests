cc478b395668c3c7f774241a61e90aa614a6c406
Jose Tello <josectello@gmail.com>
2016-01-06 11:59:02 -0800

    fix typo, rearrange watch task

--- jose_tello/gulpfile.js
+++ jose_tello/gulpfile.js
@@ -8,14 +8,14 @@ gulp.task('test', function() {
 });
 
 gulp.task('lint', function() {
-  return gulp.src(['**/*.js', '!node_modules/**', '!pakcage.json'])
+  return gulp.src(['**/*.js', '!node_modules/**', '!package.json'])
     .pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish'))
     .pipe(jshint.reporter('default'));
 });
 
-gulp.task('default', ['lint', 'test'], function() {
-  gulp.watch(['lib/*.js', 'test/*.js'], function() {
-    gulp.start(['lint']);
-  });
+gulp.task('watch', function() {
+  return gulp.watch(['lib/*.js', 'test/*.js'], ['lint', 'test']);
 });
+
+gulp.task('default', ['lint', 'test']);
