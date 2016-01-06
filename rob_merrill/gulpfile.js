28fd31d3056a2b3f27af3b507dbad5cbe54c051b
Rob Merrill <robgmerrill@gmail.com>
2016-01-06 10:45:00 -0800

    modified gulpfile

--- rob_merrill/gulpfile.js
+++ rob_merrill/gulpfile.js
@@ -3,9 +3,22 @@ var eslint = require('gulp-eslint');
 var files = ['**/*.js', '!node_modules/**'];
 gulp.task('lint', function(){
   return gulp.src(files)
-    .pipe(eslint())
+    .pipe(eslint({
+        extends: 'eslint:recommended',
+        ecmaFeatures: {
+            'modules': true
+        },
+        "rules": {
+            "no-console": 0
+        },
+        envs: [
+            'node',
+            'mocha'
+        ]
+    }))
     .pipe(eslint.format());
 
 });
 
 gulp.task('default', ['lint']);
+
