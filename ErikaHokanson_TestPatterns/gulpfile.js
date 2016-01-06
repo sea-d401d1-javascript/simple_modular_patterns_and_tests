5ab37f86e5cb1a4d45baa424675a0590b7d4d98a
ERIKAWHO <Erika@RefreshMediaResources.com>
2016-01-06 10:48:40 -0800

    Set up package.json and related updates

--- ErikaHokanson_TestPatterns/gulpfile.js
+++ ErikaHokanson_TestPatterns/gulpfile.js
@@ -1,5 +1,18 @@
-var gulp = require('gulp');
+var gulp = require('gulp'); // eslint-disable-line
+var eslint = require('gulp-eslint'); // eslint-disable-line
+var mocha = require('gulp-mocha'); // eslint-disable-line
 
-gulp.task('default', function() {
-  // place code for your default task here
+var files = ['gulpfile.js', 'greet.js', 'hello_world.js', './test/greet-test.js'];
+gulp.task('lint', function() {
+  return gulp.src(files)
+    .pipe(eslint())
+    .pipe(eslint.format());
 });
+
+gulp.task('mocha', function () {
+  return gulp.src('./test/greet-test.js', {read: false})
+		// gulp-mocha needs filepaths so you can't have any plugins before it
+		.pipe(mocha({reporter: 'nyan'}));
+});
+
+gulp.task('default', ['lint', 'mocha']);
