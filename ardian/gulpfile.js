4215fddca347660a9fbfaf64d02235abc400bf41
Ardian Ajvazi <ardian_46@hotmail.com>
2016-01-06 09:54:20 -0800

    using jshint instead.

--- ardian/gulpfile.js
+++ ardian/gulpfile.js
@@ -1,11 +1,11 @@
+var jshint = require('gulp-jshint');
 var gulp = require('gulp');
-var eslint = require('gulp-eslint');
 
 var files = ['gulpfile.js', './lib/greet.js', 'hello.js', './test/hello_test.js'];
 gulp.task('lint', function() {
   return gulp.src(files)
-   .pipe(eslint())
-    .pipe(eslint.format());
+   .pipe(jshint())
+    .pipe(jshint.reporter());
 });
 
 gulp.task('default', ['lint']);
