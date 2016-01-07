8bc799074cc732cf061eebea4596ccfce61e0be8
Ardian Ajvazi <ardian_46@hotmail.com>
2016-01-07 11:24:52 -0800

    changed from jshint to eslint

--- ardian/test/hello_test.js
+++ ardian/test/hello_test.js
@@ -1,52 +1,31 @@
 var hello = require(__dirname +  '/../hello');
 var expect = require('chai').expect;
 
-describe('hello function', function() {
-  beforeEach(function(){
+describe('hello function', function() {//eslint-disable-line
+  beforeEach(function(){//eslint-disable-line
     this.processBackup = process.argv;
     process.argv = ['node', 'hello.js'];
   });
 
-  afterEach(function() {
+  afterEach(function() {//eslint-disable-line
     process.argv = this.processBackup;
   });
 
-  it('should greet the world with no process.argv', function() {
+  it('should greet the world with no process.argv', function() {//eslint-disable-line
     expect(hello()).to.eql('hello world');
   });
 });
 
-  describe('with a name specifies', function() {
-    beforeEach(function() {
+  describe('with a name specifies', function() {//eslint-disable-line
+    beforeEach(function() {//eslint-disable-line
       this.processBackup = process.argv;
       process.argv = ['node', 'hello.js', 'test'];
     });
-    afterEach(function(){
+    afterEach(function(){//eslint-disable-line
       process.argv = this.processBackup;
     });
 
-    it('shoutld greet test', function() {
+    it('shoutld greet test', function() {//eslint-disable-line
       expect(hello()).to.eql('hello test');
     });
-});
-
-
-
-
-
-
-
-
-// process.argv[2] = 'Ardian'
-//
-// describe('the greet function', function(){
-//   it('should greet a name', function(){
-//     expect(greet('Ardian')).to.eql('Hello Ardian');
-//   });
-// });
-//
-// describe('the command line greet', function(){
-//   it('should greet a name', function(){
-//     expect(greet(process.argv[2])).to.eql('Hello Ardian');
-//   });
-// });
+  });
